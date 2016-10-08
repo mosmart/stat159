c3fe69293945d2afac48bac2d02311a991970e56
mosmart <msmart@berkeley.edu>
2016-10-08 08:16:05 -0700

    removed ggplot2 from library list

---
+++ hws/stat159-fall2016-hw03/code/scripts/session-info-script.R
@@ -0,0 +1,8 @@
+# load required packages
+## if you don't have this package, first run: install.packages("ggplot2")
+library(stargazer)
+
+# Session Info
+sink("../../session-info.txt")
+sessionInfo()
+sink()
