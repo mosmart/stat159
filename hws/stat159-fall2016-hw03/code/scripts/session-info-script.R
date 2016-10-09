c9e7ec8f6dcb5d39572d78024c5297bed0984ee3
mosmart <msmart@berkeley.edu>
2016-10-09 10:35:39 -0700

    added  as a required library

--- hws/stat159-fall2016-hw03/code/scripts/session-info-script.R
+++ hws/stat159-fall2016-hw03/code/scripts/session-info-script.R
@@ -1,6 +1,8 @@
 # load required packages
-## if you don't have this package, first run: install.packages("ggplot2")
+## if you don't have this package, first run: install.packages("stargazer")
 library(stargazer)
+## if you don't have this package, first run: install.packages("testthat")
+library(testthat)
 
 # Session Info
 sink("../../session-info.txt")
